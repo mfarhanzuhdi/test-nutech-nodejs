1a3b0a7584cafd30779fe3d47f6067cc09bccd93
Zuhdi <farhanzuhdiaan@gmail.com>
2025-06-21 19:12:09 +0700

    fix api name

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,7 +13,7 @@ const upload = require('../middlewares/uploadMiddleware');
 router.post('/registration', authController.register);
 router.post('/login', authController.login);
 router.get('/profile', verifyToken, authController.profile);
-router.put('/update', verifyToken, authController.updateProfile);
+router.put('/profile/update', verifyToken, authController.updateProfile);
 router.put('/profile/image', verifyToken, upload.single('file'), uploadProfileImage);
 router.get('/banner', bannerController.getBanners);
 router.get('/services', verifyToken, serviceController.getServices);
